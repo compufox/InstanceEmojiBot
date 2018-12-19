2f2b53ee08f284e990d3c79284da45436a1901af
Zac <thezacattacks@gmail.com>
2018-12-19 02:46:13 -0500

    added helper function to download emoji

--- helpers.rb
+++ helpers.rb
@@ -2,9 +2,15 @@ require 'net/http'
 require 'json'
 
 EmojiEndpoint = '/api/v1/custom_emojis'
+DownloadDir = '/tmp/'
 
 def get_emoji_list instance
   JSON.parse(
     Net::HTTP.get(
       URI.parse(instance + EmojiEndpoint)))
+end
+
+def get_emoji emoji
+  File.write("#{DownloadDir}#{emoji['shortcode']}.png",
+             Net::HTTP.get(URI.parse(emoji['url'])))
 end
