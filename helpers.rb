bcd2e517dfabf97c1e2176e9913b5cb077f4f926
Zac <thezacattacks@gmail.com>
2018-12-19 16:28:34 -0500

    finished main logic?
    added helper functions to save/load known instances

--- helpers.rb
+++ helpers.rb
@@ -15,4 +15,16 @@ end
 def get_emoji emoji
   File.write("#{DownloadDir}#{emoji['shortcode']}.png",
              Net::HTTP.get(URI.parse(emoji['url'])))
+
+def check_if_instance instance
+  Net::HTTP.get(URI.parse("https://#{instance}#{InstanceEndpoint}")).empty?
+end
+
+def save_instance_list
+  File.write(InstanceSave,
+             JSON.generate(known_instances))
+end
+
+def load_instance_list
+  known_instances = JSON.parse(File.read(InstanceSave)) if File.exists? InstanceSave
 end
--- main.rb
+++ main.rb
@@ -9,13 +9,35 @@ known_instances = [ 'botsin.space', 'social.computerfox.xyz',
                     'berries.space'
                   ]
 
+# load instances we have saved
+load_instance_list
+
 # if someone @s us with an instance name
-EmojiBot.on_reply do |bot|
-  
+EmojiBot.on_reply do |bot, status|
+  instance = status.content
+               .sub(/@#{bot.username} /, '')
+               .sub(/https?:\/\//, '')
+               .strip
+
+
+  unless known_instances.include?(instance) and check_if_instance(instance)
+    known_instances.append(instance)
+    
+    save_inst_list
+  end
 end
 
 
 # select a random instance from a db(?) from followers(????)
 EmojiBot.run do |bot|
+  instance = known_instances.sample
+  emojis = get_emoji_list instance
+  chosen_emoji = emojis[emoji.keys[rand(emojis.size)]]
   
+  epath = get_emoji chosen_emoji
+  bot.post("#{chosen_emoji['shortcode']} from #{instance}",
+           media: [ epath ],
+           spoiler: 'emoji',
+           hide_media: true
+          )
 end
