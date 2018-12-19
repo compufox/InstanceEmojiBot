c5cc2c8d4af4d2d854b13fd432f3a18533455a24
Zac <thezacattacks@gmail.com>
2018-12-19 17:02:13 -0500

    fixed emoji list being treated as hash instead of array
    updated starting list of instances

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
 require 'elephrame'
 require_relative 'helpers'
 
-EmojiBot = Elephrame::Bots::PeriodicInteract.new '4h'
-known_instances = [ 'botsin.space', 'social.computerfox.xyz',
-                    'glaceon.social', 'cybre.space', 'trollia.space',
-                    'queer.party', 'donphan.social', 'sleeping.town',
-                    'vulpine.club', 'yiff.life', 'lamp.institute',
-                    'berries.space'
+EmojiBot = Elephrame::Bots::PeriodInteract.new '4h'
+known_instances =  [ 'botsin.space', 'social.computerfox.xyz',
+                     'glaceon.social', 'cybre.space', 'trollian.space',
+                     'queer.party', 'donphan.social', 'sleeping.town',
+                     'vulpine.club', 'yiff.life', 'lamp.institute',
+                     'berries.space', 'computerfairi.es'
                   ]
 
 # load instances we have saved
@@ -20,10 +20,22 @@ EmojiBot.on_reply do |bot, status|
                .strip
 
 
-  unless known_instances.include?(instance) and check_if_instance(instance)
+  unless known_instances.include?(instance)
     known_instances.append(instance)
+    save_instance_list known_instances
+  end
+
+  if check_if_instance(instance)
     
-    save_inst_list
+    emojis = get_emoji_list instance
+    chosen_emoji = emojis.sample
+  
+    epath = get_emoji chosen_emoji
+    bot.reply("#{chosen_emoji['shortcode']} from #{instance}",
+             media: [ epath ],
+             spoiler: 'emoji',
+             hide_media: true
+             )
   end
 end
 
@@ -32,7 +44,7 @@ end
 EmojiBot.run do |bot|
   instance = known_instances.sample
   emojis = get_emoji_list instance
-  chosen_emoji = emojis[emoji.keys[rand(emojis.size)]]
+  chosen_emoji = emojis.sample
   
   epath = get_emoji chosen_emoji
   bot.post("#{chosen_emoji['shortcode']} from #{instance}",
