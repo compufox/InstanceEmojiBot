e2f3f7e88a73ff0d38c1bad66ae44d5e52c7fa82
Zac <thezacattacks@gmail.com>
2018-12-23 20:56:55 -0500

    moved emoji logic into it's own function
    added a check to remove instances with no emojis

--- main.rb
+++ main.rb
@@ -1,17 +1,47 @@
 require 'elephrame'
 require_relative 'helpers'
 
-EmojiBot = Elephrame::Bots::PeriodInteract.new '4h'
+EmojiBot = Elephrame::Bots::PeriodInteract.new '3h'
 known_instances =  [ 'botsin.space', 'social.computerfox.xyz',
                      'glaceon.social', 'cybre.space', 'trollian.space',
                      'queer.party', 'donphan.social', 'sleeping.town',
                      'vulpine.club', 'yiff.life', 'lamp.institute',
-                     'berries.space', 'computerfairi.es'
+                     'berries.space', 'computerfairi.es', 'chomp.life'
                   ]
 
 # load instances we have saved
 loaded = load_instance_list
 known_instances = loaded unless loaded.nil?
+
+def post_emoji(instance, bot, status = nil)
+  emojis = get_emoji_list instance
+  
+  if emojis.empty?
+    known_instances.remove! instance
+    save_instance_list known_instances
+    return false
+  end
+  
+  chosen_emoji = emojis.sample
+  epath = get_emoji chosen_emoji
+
+  if status.nil?
+    bot.post("#{chosen_emoji['shortcode']} from #{instance}",
+             media: [ epath ],
+             spoiler: 'emoji',
+             hide_media: true,
+             visibility: 'public'
+            )
+  else
+    bot.reply("#{chosen_emoji['shortcode']} from #{instance}",
+              media: [ epath ],
+              spoiler: 'emoji',
+              hide_media: true
+             )
+  end
+
+  File.delete(epath)
+end
 
 # if someone @s us with an instance name
 EmojiBot.on_reply do |bot, status|
@@ -30,35 +60,21 @@ EmojiBot.on_reply do |bot, status|
       save_instance_list known_instances
     end
     
-    emojis = get_emoji_list instance
-    chosen_emoji = emojis.sample
-  
-    epath = get_emoji chosen_emoji
-    bot.reply("#{chosen_emoji['shortcode']} from #{instance}",
-             media: [ epath ],
-             spoiler: 'emoji',
-             hide_media: true
-             )
-
-    File.delete(epath)
-  else
-    bot.reply("I don't think that's an instance :/ please check you have the correct domain name and try again!")
+    unless post_emoji(instance, bot, status)
+      bot.reply("I don't think that instance has any emoji :O")
+    end
+    
+    else
+      bot.reply("I don't think that's an instance :/ please check you have the correct domain name and try again!")
   end
 end
 
 
 # select a random instance from a db(?) from followers(????)
 EmojiBot.run do |bot|
-  instance = known_instances.sample
-  emojis = get_emoji_list instance
-  chosen_emoji = emojis.sample
-  
-  epath = get_emoji chosen_emoji
-  bot.post("#{chosen_emoji['shortcode']} from #{instance}",
-           media: [ epath ],
-           spoiler: 'emoji',
-           hide_media: true
-          )
-
-  File.delete(epath)
+  posted = false
+  while not posted
+    instance = known_instances.sample
+    post_emoji(instance, bot)
+  end
 end
